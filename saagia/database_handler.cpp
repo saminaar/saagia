8870f2eb091e0950363d2d557c0a5e1662e54789
Sami Naarminen <sami.naarminen@tuni.fi>
2021-04-14 16:27:42 +0300

    Modify database handler

--- saagia/database_handler.cpp
+++ saagia/database_handler.cpp
@@ -1,18 +1,64 @@
 #include "database_handler.h"
 #include "data_structures.h"
 
+#include <QCborMap>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QTextStream>
+
 Database_handler::Database_handler(std::shared_ptr<Data_structures> data_structures) :
     data_structures_{ data_structures }
 {
 
 }
 
-void Database_handler::save_data(QString start_time, int data_type)
+// This is copied from savegame example, has to be modified further for our use
+
+bool Database_handler::load_data()
+{
+    QFile loadFile(QStringLiteral("save.json"));
+
+    if (!loadFile.open(QIODevice::ReadOnly)) {
+        qWarning("Couldn't open save file.");
+        return false;
+    }
+
+    QByteArray saveData = loadFile.readAll();
+
+    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
+
+    read(loadDoc.object());
+
+    QTextStream(stdout) << "Loaded save for "
+                        << loadDoc["player"]["name"].toString()
+                        << " using "
+                        << "JSON" << "...\n";
+    return true;
+}
+
+bool Database_handler::save_data(QString start_time, int data_type) const
+{
+    QFile saveFile(QStringLiteral("save.json"));
+
+    if (!saveFile.open(QIODevice::WriteOnly)) {
+        qWarning("Couldn't open save file.");
+        return false;
+    }
+
+    QJsonObject gameObject;
+    write(gameObject);
+    saveFile.write(QJsonDocument(gameObject).toJson());
+
+    return true;
+}
+
+void Database_handler::read(const QJsonObject &json)
 {
 
 }
 
-void Database_handler::load_data()
+void Database_handler::write(QJsonObject &json) const
 {
 
 }
--- saagia/database_handler.h
+++ saagia/database_handler.h
@@ -1,6 +1,7 @@
 #ifndef DATABASE_HANDLER_H
 #define DATABASE_HANDLER_H
 
+#include <QJsonObject>
 #include <QString>
 
 class Data_structures;
@@ -9,11 +10,14 @@ class Database_handler
 {
 
 public:
+
     Database_handler(std::shared_ptr<Data_structures> data_structures);
 
-    void save_data(QString start_time, int data_type);
+    bool load_data();
+    bool save_data(QString start_time, int data_type) const;
 
-    void load_data();
+    void read(const QJsonObject &json);
+    void write(QJsonObject &json) const;
 
 private:
     std::shared_ptr<Data_structures> data_structures_;
